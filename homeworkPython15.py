833598a8d520e2a499b1bb9cd1fa8cc9f5d28efd
VladislavBelmas <vladislav.belmas1@gmail.com>
2026-03-01 02:04:38 +0100

    added new solution method

--- homeworkPython15.py
+++ homeworkPython15.py
@@ -13,9 +13,19 @@ products = [["Laptop", 1200], ["Mouse", 25], ["Keyboard", 75], ["Monitor", 200]]
 discount = 0.17#int(input("Enter the discount %: ")) / 100
 result = [f'{"Товар":<14} {"Старая цена":>10} {"Новая цена":>15}']
 
+for item in products:
+    product, price = item
+    new_price = price - price * discount
+    item.append(new_price)
+    result.append(f"{product:<14} {price:>10.2f}$ {new_price:>14.2f}$")
+
+print("\n".join(result))
+
+#-----------------------------------------------------------------------------------------------------------------------
+#Изначальный вариант решения
+
 for product in products:
     product.append(product[1] - product[1] * discount)
     result.append(f"{product[0]:<14} {product[1]:>10.2f}$ {product[2]:>14.2f}$")
 
 print("\n".join(result))
-

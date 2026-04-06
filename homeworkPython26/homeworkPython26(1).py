9c52e1858c4beb82e6d045cf622312bc13d957f4
VladislavBelmas <vladislav.belmas1@gmail.com>
2026-04-06 20:32:23 +0200

    add homework 26

---
+++ homeworkPython26/homeworkPython26(1).py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+if len(sys.argv) != 2:
+    print("Enter only 1 argument")
+    sys.exit(1)
+
+path = sys.argv[1]
+folders = ['Folders:']
+files = ['Files:']
+
+
+if not os.path.exists(path):
+    print('Enter correct path')
+    sys.exit(1)
+
+
+os.chdir(path)
+
+
+for item in os.listdir(path):
+
+    if os.path.isfile(item):
+        files.append(f'- {item}')
+
+    elif os.path.isdir(item):
+        folders.append(f'- {item}')
+
+
+print('\n'.join(folders),'\n')
+print('\n'.join(files))
+
+
+
+
+
+
+
+
---
+++ homeworkPython26/homeworkPython26(2).py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+if len(sys.argv) != 3:
+    print('Expected only 3 arguments (script name, "path", file type)')
+    sys.exit(1)
+
+path = sys.argv[1]
+file_type = sys.argv[2]
+result = [f'Files with type "{file_type}":']
+
+if not os.path.exists(path):
+    print('Enter correct path')
+    sys.exit(1)
+
+
+os.chdir(path)
+
+
+for root, dirs, files in os.walk(path):
+    for file in files:
+        if os.path.basename(file).endswith(file_type):
+            result.append(os.path.join(root,file))
+
+print('\n- '.join(result))
+
+
+if not result[1:]:
+    print("Not found")
+    sys.exit(1)
+
+
+action = input("Wanna delete? (y/n): ")
+
+
+if action in ('y', 'yes'):
+    for file in result[1:]:
+       os.remove(os.path.abspath(file))
+    else:
+        print("Deleted")
+
+else:
+    print("Script end's")

86f21ae4b618051fb3caab9fb5caee4505356fb1
VladislavBelmas <vladislav.belmas1@gmail.com>
2026-04-06 17:12:16 +0200

    add new solution

--- homeworkPython25.py
+++ homeworkPython25.py
@@ -33,3 +33,30 @@ print(divide(dividend, divider))
 
 
 
+
+    :param div1: dividend (what we divide)
+    :param div2: divider (what do we divide into)
+    :return: division result
+    """
+
+    import logging
+    logging.basicConfig(filename='errors.log', level=logging.ERROR, format="%(asctime)s) - %(levelname)s - "
+                                                     "%(filename)s - %(lineno)s - %(message)s")
+
+    if not div1.isnumeric() or not div2.isnumeric():
+
+        excep = "Ошибка: Введено некорректное число."
+        raise ValueError(excep)
+        logging.error(excp)
+
+    if int(div2) == 0:
+
+        excep = "Ошибка: Нельзя делить на ноль."
+        raise ZeroDivisionError(excep)
+        logging.error(excp)
+
+    result = int(div1) / int(div2)
+    return result
+
+
+print(divide2(dividend, divider))

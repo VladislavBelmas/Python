8fed85a3f020ab157e8e2eb3d727f72e51d5e8f6
VladislavBelmas <vladislav.belmas1@gmail.com>
2026-04-10 10:06:50 +0200

    delete first func

--- homeworkPython25.py
+++ homeworkPython25.py
@@ -15,37 +15,6 @@ def divide (div1:str|int|float , div2:str|int|float) -> str|float:
     import logging
     logging.basicConfig(filename='errors.log', level=logging.ERROR, format="%(asctime)s) - %(levelname)s - "
                                                      "%(filename)s - %(lineno)s - %(message)s")
-    try:
-        result = int(div1) / int(div2)
-
-    except ValueError:
-        result = "Ошибка: Введено некорректное число."
-        logging.error(result)
-
-    except ZeroDivisionError:
-        result = "Ошибка: Нельзя делить на ноль."
-        logging.error(result)
-
-    return result
-
-
-print(divide(dividend, divider))
-
-
-
-def divide2 (div1:str|int|float , div2:str|int|float) -> str|float:
-    """
-    Returns the result of dividing two numbers or an error.
-
-
-    :param div1: dividend (what we divide)
-    :param div2: divider (what do we divide into)
-    :return: division result
-    """
-
-    import logging
-    logging.basicConfig(filename='errors.log', level=logging.ERROR, format="%(asctime)s) - %(levelname)s - "
-                                                     "%(filename)s - %(lineno)s - %(message)s")
 
     if not div1.isnumeric() or not div2.isnumeric():
 
@@ -63,4 +32,4 @@ def divide2 (div1:str|int|float , div2:str|int|float) -> str|float:
     return result
 
 
-print(divide2(dividend, divider))
+print(divide(dividend, divider))

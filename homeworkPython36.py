e7c15788eab8036371d3fa261eae1c79ef775a47
VladislavBelmas <vladislav.belmas1@gmail.com>
2026-05-06 14:52:52 +0200

    add homeworkPython36.py

---
+++ homeworkPython36.py
@@ -0,0 +1,33 @@
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def introduce(self):
+        print(f"Hello, my name is {self.name}")
+
+
+class Student(Person):
+    def __init__(self, name, course_num):
+        super().__init__(name)
+        self.course_num = course_num
+
+    def introduce(self):
+        super().introduce()
+        print(f"I'm on course {self.course_num}")
+
+
+class Teacher(Person):
+    def __init__(self, name, subject):
+        super().__init__(name)
+        self.subject = subject
+
+    def introduce(self):
+        print(f"Hello, I am professor {self.name}.\nMy subject is {self.subject}")
+
+
+student = Student("Alice", "2")
+teacher = Teacher("Bob", "Mathematics")
+
+for person in (student, teacher):
+    person.introduce()
